3bb6d145f54a1c0a0179c195de79e3ac0343a66a
730s <leonardosade@gmail.com>
2024-06-03 18:57:00 -0300

    Implementação de melhoria na exibição do tabuleiro

--- src/Xadrez/PartidaDeXadrez.java
+++ src/Xadrez/PartidaDeXadrez.java
@@ -25,7 +25,12 @@ public class PartidaDeXadrez {
 
     }
     private void setupInicial(){
-        inserirNovaPeca('a', 8, new Torre(tabuleiro, Cores.BRANCO));
+        inserirNovaPeca('a', 8, new Torre(tabuleiro, Cores.PRETO));
+        inserirNovaPeca('h', 8, new Torre(tabuleiro, Cores.PRETO));
         inserirNovaPeca('e', 8, new Rei(tabuleiro, Cores.PRETO));
+        inserirNovaPeca('a', 1, new Torre(tabuleiro, Cores.BRANCO));
+        inserirNovaPeca('h', 1, new Torre(tabuleiro, Cores.BRANCO));
+        inserirNovaPeca('e', 1, new Rei(tabuleiro, Cores.BRANCO));
+
     }
 }
